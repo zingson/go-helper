d1b91710fbc3c9ffb8d6d10fff029f2205e23048
Zengs <zengs@himkt.cn>
2023-03-18 21:19:47 +0800

    Fix

--- hurl/url.go
+++ hurl/url.go
@@ -2,20 +2,10 @@ package hurl
 
 import "strings"
 
-// Splice 链接参数拼接, 如果由#号拼接在#号后的链接上
+// Splice 链接参数拼接
 func Splice(url string, params map[string]string) string {
 	if params == nil {
 		params = make(map[string]string)
-	}
-	query := ""
-	for k, v := range params {
-		if v == "" {
-			continue
-		}
-		query += "&" + k + "=" + v
-	}
-	if len(query) == 0 {
-		return url
 	}
 
 	if strings.Contains(url, "#") {
@@ -31,6 +21,15 @@ func Splice(url string, params map[string]string) string {
 			url = url + "?"
 		}
 	}
-
+	query := ""
+	for k, v := range params {
+		if v == "" {
+			continue
+		}
+		query += "&" + k + "=" + v
+	}
+	if len(query) == 0 {
+		return url
+	}
 	return url + query[1:]
 }
