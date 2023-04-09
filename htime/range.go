9bca3bc6b96d925a5a2fa0b0790cb058fdc7bce6
Zengs <zengs@himkt.cn>
2023-04-09 19:27:45 +0800

    Fix

--- htime/range.go
+++ htime/range.go
@@ -4,20 +4,20 @@ import (
 	"time"
 )
 
-// TimeRangeDays 天时间范围  n 0=当天 -1=前一天 1=后一天
-func TimeRangeDays(n int) (beg, end time.Time) {
+// RangeDays 天时间范围  n 0=当天 -1=前一天 1=后一天
+func RangeDays(n int) (beg, end time.Time) {
 	y, m, d := time.Now().Date()
 	return time.Date(y, m, d, 0, 0, 0, 0, time.Local).AddDate(0, 0, n),
 		time.Date(y, m, d, 23, 59, 59, 0, time.Local).AddDate(0, 0, n)
 }
 
-func TimeRangeDaysF(n int, f string) (beg, end string) {
-	b, e := TimeRangeDays(n)
+func RangeDaysF(n int, f string) (beg, end string) {
+	b, e := RangeDays(n)
 	return b.Format(f), e.Format(f)
 }
 
 // TimeRangeWeek 周时间范围 n 0=本周 -1=前一周 1=后一周
-func TimeRangeWeek(n int) (beg, end time.Time) {
+func RangeWeek(n int) (beg, end time.Time) {
 	ntime := time.Now()
 	offset := int(time.Monday - ntime.Weekday())
 	// 周日time.Sunday=0 做特殊判断
@@ -29,31 +29,31 @@ func TimeRangeWeek(n int) (beg, end time.Time) {
 		time.Date(year, month, day, 23, 59, 59, 0, time.Local).AddDate(0, 0, offset+6+7*n)
 }
 
-func TimeRangeWeekF(n int, f string) (beg, end string) {
-	b, e := TimeRangeWeek(n)
+func RangeWeekF(n int, f string) (beg, end string) {
+	b, e := RangeWeek(n)
 	return b.Format(f), e.Format(f)
 }
 
-// TimeRangeMonth 月时间范围 n 0=本月 -1=前一月 1=后一月
-func TimeRangeMonth(n int) (beg, end time.Time) {
+// RangeMonth 月时间范围 n 0=本月 -1=前一月 1=后一月
+func RangeMonth(n int) (beg, end time.Time) {
 	y, m, _ := time.Now().Date()
 	return time.Date(y, m, 1, 0, 0, 0, 0, time.Local).AddDate(0, n, 0),
 		time.Date(y, m, 1, 23, 59, 59, 0, time.Local).AddDate(0, 1+n, -1)
 }
 
-func TimeRangeMonthF(n int, f string) (beg, end string) {
-	b, e := TimeRangeMonth(n)
+func RangeMonthF(n int, f string) (beg, end string) {
+	b, e := RangeMonth(n)
 	return b.Format(f), e.Format(f)
 }
 
-// TimeRangeYear 本年时间范围 n 0=本年 -1=前一年 1=后一年
-func TimeRangeYear(n int) (beg, end time.Time) {
+// RangeYear 本年时间范围 n 0=本年 -1=前一年 1=后一年
+func RangeYear(n int) (beg, end time.Time) {
 	y, _, _ := time.Now().Date()
 	return time.Date(y, 1, 1, 0, 0, 0, 0, time.Local).AddDate(n, 0, 0),
 		time.Date(y, 12, 31, 23, 59, 59, 0, time.Local).AddDate(n, 0, 0)
 }
 
-func TimeRangeYearF(n int, f string) (beg, end string) {
-	b, e := TimeRangeYear(n)
+func RangeYearF(n int, f string) (beg, end string) {
+	b, e := RangeYear(n)
 	return b.Format(f), e.Format(f)
 }
